97d22e7a4d6e6c6eee9c32816250dd64141c450b
taniaamreen <99126657+taniaamreen@users.noreply.github.com>
2025-07-11 21:00:24 -0500

    Push actual project files for face-attendance system

---
+++ db_practice.py
@@ -0,0 +1,10 @@
+import os
+
+path = "TrainingImage"
+
+# imagePath = [os.path.join(path, f) for f in os.listdir(path)]
+for d in os.listdir(path):
+    newdir = os.path.join(path, d)
+    print(newdir)
+    imagePath = [os.path.join(newdir, f) for f in os.listdir(newdir)]
+print(imagePath)
